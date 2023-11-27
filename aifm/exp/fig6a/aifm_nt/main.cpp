f78418b357a6cf38ee8584945a056a90a99b0798
Shi Liu <shiliu@g.ucla.edu>
2023-11-27 15:51:27 +0800

    feat: [web] WS AIFM version

--- aifm/exp/fig6a/aifm_nt/main.cpp
+++ aifm/exp/fig6a/aifm_nt/main.cpp
@@ -30,19 +30,33 @@ extern "C" {
 #include <numeric>
 #include <random>
 #include <vector>
+#include <fstream>
 
 using namespace far_memory;
 
 #define ACCESS_ONCE(x) (*(volatile typeof(x) *)&(x))
 
 namespace far_memory {
+
+void save_to_file(const std::string&file, std::vector<uint32_t> &data){
+    std::ofstream of;
+    of.open(file, std::iostream::binary | std::iostream::out);
+    of.write((char*)data.data(), data.size() * sizeof(uint32_t));
+    of.close();
+    printf("Save to file %s\n", file.c_str());
+}
+
 class FarMemTest {
 private:
+  constexpr static double target_mops = 1;
   // FarMemManager.
-  constexpr static uint64_t kCacheSize = 2048 * Region::kSize;
-  constexpr static uint64_t kFarMemSize = (17ULL << 30); // 17 GB
+  constexpr static uint64_t kCacheSize = 2560 * Region::kSize;
+  constexpr static uint64_t kFarMemSize = (24ULL << 30); // 20 GB
   constexpr static uint32_t kNumGCThreads = 12;
-  constexpr static uint32_t kNumConnections = 300;
+
+  // For high thread count experiments, use more connections
+  //constexpr static uint32_t kNumConnections = 300;
+  constexpr static uint32_t kNumConnections = 1600;
 
   // Hashtable.
   constexpr static uint32_t kKeyLen = 12;
@@ -57,8 +71,8 @@ private:
   constexpr static uint32_t kArrayEntrySize = 8192;     // 8 K
 
   // Runtime.
-  constexpr static uint32_t kNumMutatorThreads = 6;
-  constexpr static double kZipfParamS = 0.85;
+  constexpr static uint32_t kNumMutatorThreads = 54;
+  constexpr static double kZipfParamS = 0.8;
   constexpr static uint32_t kNumKeysPerRequest = 32;
   constexpr static uint32_t kNumReqs = kNumKVPairs / kNumKeysPerRequest;
   constexpr static uint32_t kLog10NumKeysPerRequest =
@@ -69,8 +83,8 @@ private:
   // Output.
   constexpr static uint32_t kPrintPerIters = 8192;
   constexpr static uint32_t kMaxPrintIntervalUs = 1000 * 1000; // 1 second(s).
-  constexpr static uint32_t kPrintTimes = 100;
-  constexpr static uint32_t kLatsWinSize = 1 << 12;
+  constexpr static uint32_t kPrintTimes = 50;
+  constexpr static uint32_t kLatsWinSize = 1 << 17;
 
   struct Req {
     char data[kReqLen];
@@ -98,16 +112,19 @@ private:
   std::unique_ptr<std::mt19937> generators[helpers::kNumCPUs];
   alignas(helpers::kHugepageSize) Req all_gen_reqs[kNumReqs];
   uint32_t all_zipf_req_indices[helpers::kNumCPUs][kReqSeqLen];
+  uint64_t all_zipf_req_start_times[helpers::kNumCPUs][kReqSeqLen];
 
   Cnt req_cnts[kNumMutatorThreads];
-  uint32_t lats[helpers::kNumCPUs][kLatsWinSize];
-  Cnt lats_idx[helpers::kNumCPUs];
+  uint32_t lats[kNumMutatorThreads][kLatsWinSize];
+  Cnt lats_idx[kNumMutatorThreads];
   Cnt per_core_req_idx[helpers::kNumCPUs];
+  uint32_t random_start_idx[kNumMutatorThreads];
 
   std::atomic_flag flag;
   uint64_t print_times = 0;
   uint64_t prev_sum_reqs = 0;
   uint64_t prev_us = 0;
+  int64_t bench_start_us = 0;
   std::vector<double> mops_records;
 
   unsigned char key[CryptoPP::AES::DEFAULT_KEYLENGTH];
@@ -173,7 +190,11 @@ private:
         auto num_reqs_per_thread = kNumReqs / kNumMutatorThreads;
         auto req_offset = tid * num_reqs_per_thread;
         auto *thread_gen_reqs = &all_gen_reqs[req_offset];
-        for (uint32_t i = 0; i < num_reqs_per_thread; i++) {
+        auto target_num = num_reqs_per_thread;
+        if(tid == kNumMutatorThreads - 1) {
+          target_num = kNumReqs - req_offset;
+        }
+        for (uint32_t i = 0; i < target_num; i++) {
           Req req;
           random_req(req.data, tid);
           Key key;
@@ -194,18 +215,36 @@ private:
     for (auto &thread : threads) {
       thread.Join();
     }
-    preempt_disable();
-    zipf_table_distribution<> zipf(kNumReqs, kZipfParamS);
-    auto &generator = generators[get_core_num()];
-    constexpr uint32_t kPerCoreWinInterval = kReqSeqLen / helpers::kNumCPUs;
-    for (uint32_t i = 0; i < kReqSeqLen; i++) {
-      auto rand_idx = zipf(*generator);
-      for (uint32_t j = 0; j < helpers::kNumCPUs; j++) {
-        all_zipf_req_indices[j][(i + (j * kPerCoreWinInterval)) % kReqSeqLen] =
-            rand_idx;
-      }
+
+    std::uniform_int_distribution<uint32_t> distribution(0, 
+            kReqSeqLen * (helpers::kNumCPUs - 1));
+    for (uint i = 0; i < kNumMutatorThreads; ++i){
+      random_start_idx[i] = distribution(*generators[0]); 
     }
-    preempt_enable();
+
+    zipf_table_distribution<> zipf(kNumReqs, kZipfParamS);
+    std::vector<rt::Thread> zipf_threads;
+    for (uint32_t k = 0; k < helpers::kNumCPUs; k ++){
+      zipf_threads.emplace_back(rt::Thread([&, k]{
+        uint64_t cur_us = 0;
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::exponential_distribution<double> d(target_mops / kNumMutatorThreads);
+        auto &generator = generators[k];
+        for (uint32_t i = 0; i < kReqSeqLen; i++) {
+          auto idx = zipf(*generator);
+          BUG_ON(idx >= kNumReqs);
+          all_zipf_req_indices[k][i] = idx;
+          uint64_t interval = std::max(1l, std::lround(d(gen)));
+          all_zipf_req_start_times[k][i] = cur_us;
+          cur_us += interval;
+        }
+      }));
+    }
+
+    for (auto &thread : zipf_threads) {
+      thread.Join();
+    }
   }
 
   void prepare(AppArray *array) {
@@ -236,9 +275,11 @@ private:
       if (us - prev_us > kMaxPrintIntervalUs) {
         auto mops =
             ((double)(sum_reqs - prev_sum_reqs) / (us - prev_us)) * 1.098;
+       std::cout << "mops: " << mops << "\n";
         mops_records.push_back(mops);
         us = microtime();
-        if (print_times++ >= kPrintTimes) {
+        //if (print_times++ >= kPrintTimes) {
+        if (us - bench_start_us >= kPrintTimes * 1000000) {
           constexpr double kRatioChosenRecords = 0.1;
           uint32_t num_chosen_records =
               mops_records.size() * kRatioChosenRecords;
@@ -250,14 +291,26 @@ private:
                            mops_records.size()
                     << std::endl;
           std::vector<uint32_t> all_lats;
-          for (uint32_t i = 0; i < helpers::kNumCPUs; i++) {
+          for (uint32_t i = 0; i < kNumMutatorThreads; i++) {
             auto num_lats = std::min((uint64_t)kLatsWinSize, lats_idx[i].c);
             all_lats.insert(all_lats.end(), &lats[i][0], &lats[i][num_lats]);
           }
           std::sort(all_lats.begin(), all_lats.end());
+          std::cout << "20 tail lat (cycles) = "
+                    << all_lats[all_lats.size() * 20.0 / 100] << std::endl;
+          std::cout << "40 tail lat (cycles) = "
+                    << all_lats[all_lats.size() * 40.0 / 100] << std::endl;
+          std::cout << "60 tail lat (cycles) = "
+                    << all_lats[all_lats.size() * 60.0 / 100] << std::endl;
           std::cout << "90 tail lat (cycles) = "
                     << all_lats[all_lats.size() * 90.0 / 100] << std::endl;
-          exit(0);
+          std::cout << "95 tail lat (cycles) = "
+                    << all_lats[all_lats.size() * 95.0 / 100] << std::endl;
+          std::cout << "99 tail lat (cycles) = "
+                    << all_lats[all_lats.size() * 99.0 / 100] << std::endl;
+	        save_to_file("logs/aifm-web-tempo-2560m-54t-1ol.raw", all_lats);
+          std::cout << "Done. Force exiting..." << std::endl;
+          abort();
         }
         prev_us = us;
         prev_sum_reqs = sum_reqs;
@@ -269,29 +322,38 @@ private:
 
   void bench(GenericConcurrentHopscotch *hopscotch, AppArray *array) {
     std::vector<rt::Thread> threads;
+    bench_start_us = microtime();
     prev_us = microtime();
     for (uint32_t tid = 0; tid < kNumMutatorThreads; tid++) {
-      threads.emplace_back(rt::Thread([&, tid]() {
+      uint32_t start_idx = random_start_idx[tid];
+      threads.emplace_back(rt::Thread([&, tid, start_idx]() {
         uint32_t cnt = 0;
+	      uint32_t per_thread_req_idx = 0;
+        auto start_us = microtime();
         while (1) {
           if (unlikely(cnt++ % kPrintPerIters == 0)) {
             preempt_disable();
             print_perf();
             preempt_enable();
           }
-          preempt_disable();
-          auto core_num = get_core_num();
+          auto core_num = tid % helpers::kNumCPUs;
           auto req_idx =
-              all_zipf_req_indices[core_num][per_core_req_idx[core_num].c];
-          if (unlikely(++per_core_req_idx[core_num].c == kReqSeqLen)) {
-            per_core_req_idx[core_num].c = 0;
+              ((uint32_t*)all_zipf_req_indices)[start_idx+per_thread_req_idx];
+          auto start_time = all_zipf_req_start_times[core_num][per_thread_req_idx];
+          auto relative_us = microtime() - start_us;
+          if (unlikely(++per_thread_req_idx == kReqSeqLen)) {
+            per_thread_req_idx = 0;
           }
-          preempt_enable();
+          if (start_time > relative_us) {
+            timer_sleep(start_time - relative_us);
+          }// else if (start_time + miss_ddl_thresh_us < relative_us) {
+          //    continue;
+          //}
 
           auto &req = all_gen_reqs[req_idx];
           Key key;
           memcpy(key.data, req.data, kReqLen);
-          auto start = rdtsc();
+          //auto start = rdtsc();
           uint32_t array_index = 0;
           {
             DerefScope scope;
@@ -308,18 +370,25 @@ private:
           {
             array_index %= kNumArrayEntries;
             DerefScope scope;
+            // NT disabled
             const auto &array_entry =
-                array->at</* NT = */ true>(scope, array_index);
+                array->at_mut</* NT = */ true>(scope, array_index);
             preempt_disable();
             consume_array_entry(array_entry);
+          lats[tid][(lats_idx[tid].c++) % kLatsWinSize] = microtime() - (start_time + start_us);
+          ACCESS_ONCE(req_cnts[tid].c)++;
             preempt_enable();
           }
-          auto end = rdtsc();
-          preempt_disable();
-          core_num = get_core_num();
-          lats[core_num][(lats_idx[core_num].c++) % kLatsWinSize] = end - start;
-          preempt_enable();
-          ACCESS_ONCE(req_cnts[tid].c)++;
+          //auto end = rdtsc();
+
+          //lats[tid][(lats_idx[tid].c++) % kLatsWinSize] = end - start;
+          cnt++;
+	  // if(cnt % 8 == 0){
+	  //    thread_yield();
+	  // }
+          if (unlikely(per_thread_req_idx == 0)) {
+              start_us += all_zipf_req_start_times[core_num][kReqSeqLen-1];
+          }
         }
       }));
     }
--- aifm/exp/fig6a/aifm_nt/run.sh
+++ aifm/exp/fig6a/aifm_nt/run.sh
@@ -2,16 +2,28 @@
 
 source ../../../shared.sh
 
-num_threads_arr=(3 4 5 6 7 8 12 13 14 20 30)
 
+num_threads_arr=(54)
+target_mops_arr=(1)
+local_mem_arr=(2560 5120 10240)
 sudo pkill -9 main
-
 for num_threads in ${num_threads_arr[@]}
 do
-    sed "s/constexpr static uint32_t kNumMutatorThreads.*/constexpr static uint32_t kNumMutatorThreads = $num_threads;/g" main.cpp -i
-    make clean
-    make -j
-    rerun_local_iokerneld_args simple 1,2,3,4,5,6,7,8,9,11,12,13,14,15
-    rerun_mem_server
-    run_program ./main | grep "mops\|tail" 1>log.$num_threads 2>&1
+    for target_mops in ${target_mops_arr[@]}
+    do
+        for local_mem in ${local_mem_arr[@]}
+        do
+            log_name=aifm-web-tempo-${local_mem}m-${num_threads}t-${target_mops}ol
+            sed "s/constexpr static uint64_t kCacheSize = .*/constexpr static uint64_t kCacheSize = $local_mem * Region::kSize;/g" main.cpp -i
+            sed "s/constexpr static double target_mops =.*/constexpr static double target_mops = $target_mops;/g" main.cpp -i
+            sed "s/static uint32_t kNumMutatorThreads = .*/static uint32_t kNumMutatorThreads = $num_threads;/g" main.cpp -i
+            sed "s|save_to_file(\".*\.raw\", all_lats);|save_to_file(\"logs/$log_name\.raw\", all_lats);|g" main.cpp -i
+            make clean
+            make -j
+            rerun_local_iokerneld
+            rerun_mem_server
+            run_program ./main | tee -a logs/${log_name}
+        done
+    done
 done
+kill_local_iokerneld
